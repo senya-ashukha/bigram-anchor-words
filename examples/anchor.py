6ac659757e2b87d430dada168afa610b84c27d9c
Arseniy Ashuha <ars.ashuha@gmail.com>
2015-04-12 12:31:21 +0300

    profile anchors 1

--- examples/anchor.py
+++ examples/anchor.py
@@ -6,10 +6,14 @@ from tmtk.collection.collection import FullTextCollection
 from tmtk.collection.transformer_api import TransformerChainApply
 from tmtk.collection.transformer import BigramExtractorDocumentsTransform
 
+import cProfile
+
 collection = FullTextCollection(path='./tmtk/corpa/ru_bank_wid_small.zip').fill()
 
+cProfile.run('''
 F, anc = anchor.anchor_model(collection.documents_train, collection.documents_test,
                              wrd_count=len(collection.id_to_words),
                              metrics=[preplexity, coherence, uniq_top_of_topics])
+''')
 
 anchor.print_topics(F, collection.id_to_words, anc)
