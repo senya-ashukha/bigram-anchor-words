ceb8d0387cb2fcaf1fefdd4494661a5f7e381868
Arseniy <ars-ashuha@yandex.ru>
2015-04-12 15:49:44 +0300

    bit

--- examples/plsa.py
+++ examples/plsa.py
@@ -8,10 +8,10 @@ from tmtk.collection.transformer import BigramExtractorDocumentsTransform
 
 
 collection = FullTextCollection(path='./tmtk/corpa/ru_bank_wid_small.zip').fill()
-
+'''
 transformers = TransformerChainApply(transformers=[BigramExtractorDocumentsTransform()])
 collection = transformers.apply(collection)
-
+'''
 F, T = plsa.plsa_model(collection.documents_train, collection.documents_test,
                    wrd_count=len(collection.id_to_words),
                    metrics=[preplexity, coherence, uniq_top_of_topics],
