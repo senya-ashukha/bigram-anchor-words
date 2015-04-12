4440e686494cd081247d3e5e18f82ea60411016c
Arseniy Ashuha <ars.ashuha@gmail.com>
2015-04-12 12:27:12 +0300

    profile anchors

--- examples/plsa.py
+++ examples/plsa.py
@@ -7,20 +7,22 @@ from tmtk.collection.transformer_api import TransformerChainApply
 from tmtk.collection.transformer import BigramExtractorDocumentsTransform
 
 
+import cProfile
+
+
 collection = FullTextCollection(path='./tmtk/corpa/ru_bank_wid_small.zip').fill()
 
-applyer = TransformerChainApply(
-    transformers=[BigramExtractorDocumentsTransform()])
+transformers = TransformerChainApply(transformers=[BigramExtractorDocumentsTransform()])
 
-'''
-collection_train = applyer.apply(collection_train)
-collection_test.bigrams = collection_train.bigrams
-collection_test = transformers[0].apply(collection_test)
-'''
+collection = transformers.apply(collection)
 
-F, T = plsa.plsa_model(collection.documents_train, collection.documents_test,
+cProfile.run(
+    '''
+    F, T = plsa.plsa_model(collection.documents_train, collection.documents_test,
                        wrd_count=len(collection.id_to_words),
                        metrics=[preplexity, coherence, uniq_top_of_topics],
                        num_iter=40, verbose=False)
+    '''
+)
 
 plsa.print_topics(F, collection.id_to_words)
