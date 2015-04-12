4c53bb3eea3ddf1f96f0aa1e882a1cb74719eea6
Arseniy <ars-ashuha@yandex.ru>
2015-04-12 15:30:57 +0300

    bit

--- examples/z_anchor_plsa_mix.py
+++ examples/z_anchor_plsa_mix.py
@@ -12,6 +12,6 @@ F, anc = anchor.anchor_model(collection.documents_train, collection.documents_te
 F, T = plsa.plsa_model(collection.documents_train, collection.documents_test,
                        wrd_count=len(collection.id_to_words),
                        metrics=[preplexity, coherence, uniq_top_of_topics],
-                       num_iter=30, verbose=False, F=F)
+                       num_iter=40, verbose=False, F=F)
 
 plsa.print_topics(F, collection.id_to_words)
