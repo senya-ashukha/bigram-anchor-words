719349cfc6ff06de8e5408b0888899482eb22845
Arseniy Ashuha <ars.ashuha@gmail.com>
2015-04-12 23:50:57 +0300

    bit

--- examples/z_anchor_plsa_mix.py
+++ examples/z_anchor_plsa_mix.py
@@ -12,12 +12,12 @@ transformers = TransformerChainApply(transformers=[BigramExtractorDocumentsTrans
 collection = transformers.apply(collection)
 
 F, anc = anchor.anchor_model(
-    collection.documents_train,
+    collectionW,
     wrd_count=len(collection.id_to_words),
     metrics=[preplexity, coherence, uniq_top_of_topics])
 
 F, T = plsa.plsa_model(
-    collection.documents_train,
+    collection,
     wrd_count=len(collection.id_to_words),
     metrics=[preplexity, coherence, uniq_top_of_topics],
     num_iter=15, verbose=False, F=F)
