477da6076927130ce84c035470c5dbccc01324a1
Arseniy <ars-ashuha@yandex.ru>
2015-04-14 23:23:00 +0300

    bit

--- examples/z_anchor_plsa_mix.py
+++ examples/z_anchor_plsa_mix.py
@@ -3,20 +3,24 @@ from tmtk.topic_models import anchor, plsa
 from tmtk.metrics.metrics import preplexity, coherence, uniq_top_of_topics
 from tmtk.collection.collection import FullTextCollection
 
+'''
 from tmtk.collection.transformer_api import TransformerChainApply
 from tmtk.collection.transformer import BigramExtractorDocumentsTransform
+'''
 
 collection = FullTextCollection(path='./tmtk/corpa/ru_bank_wid_small.zip').fill()
 
+'''
 transformers = TransformerChainApply(transformers=[BigramExtractorDocumentsTransform()])
 collection = transformers.apply(collection)
+'''
 
 F, anc = anchor.anchor_model(
     collection,
     wrd_count=len(collection.id_to_words),
     metrics=[preplexity, coherence, uniq_top_of_topics])
 
-anchor.print_topics(F, collection.id_to_words, anc, 'bigr_anch_topics.txt')
+anchor.print_topics(F, collection.id_to_words, anc, 'anch_topics.txt')
 
 F, T = plsa.plsa_model(
     collection,
@@ -24,4 +28,4 @@ F, T = plsa.plsa_model(
     metrics=[preplexity, coherence, uniq_top_of_topics],
     num_iter=40, verbose=False, F=F)
 
-plsa.print_topics(F, collection.id_to_words, 'bigr_anch_plsa_topics.txt')
+plsa.print_topics(F, collection.id_to_words, 'anch_plsa_topics.txt')
