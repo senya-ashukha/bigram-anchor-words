c2554f25b5278a59b0f9e4d8f4f273916cbce61a
Arseniy <ars-ashuha@yandex.ru>
2015-04-16 10:08:41 +0300

    bit

--- tmtk/topic_models/utils.py
+++ tmtk/topic_models/utils.py
@@ -5,3 +5,9 @@ def get_topic(word_topic, topic, head=10):
     col = map(itemgetter(0), sorted(col, key=itemgetter(1), reverse=True)[:head])
 
     return col
+
+def topic(topic, id_to_wrd, head=10, verb=True):
+    col = list(enumerate(topic))
+    col = map(itemgetter(0), sorted(col, key=itemgetter(1), reverse=True)[:head])
+    if verb: print ' '.join(map(lambda x: id_to_wrd[x], col)).encode('utf-8') + '\n'
+    return col
