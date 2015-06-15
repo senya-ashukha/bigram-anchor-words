541c747d1613b4a005c4437a0bb539f717122f3b
Arseniy Ashuha <ars.ashuha@gmail.com>
2015-06-15 23:41:00 +0300

    add bigramm to anch

--- examples/ru_bank_wid_small/an+pl+bi.py
+++ examples/ru_bank_wid_small/an+pl+bi.py
@@ -3,12 +3,18 @@ from tmtk.topic_models import anchor, plsa
 from tmtk.metrics.metrics import preplexity, coherence, uniq_top_of_topics
 from tmtk.collection.collection import FullTextCollection
 
+from tmtk.collection.transformer_api import TransformerChainApply
+from tmtk.collection.transformer import BigramExtractorDocumentsTransform
+
 collection = FullTextCollection(path='./tmtk/corpa/ru_bank_wid_small.zip').fill()
+
+transformers = TransformerChainApply(transformers=[BigramExtractorDocumentsTransform(do_apply=False)])
+collection = transformers.apply(collection)
 
 F, anc = anchor.anchor_model(
     collection,
     wrd_count=len(collection.id_to_words),
-    metrics=[preplexity, coherence, uniq_top_of_topics])
+    metrics=[preplexity, coherence, uniq_top_of_topics], bi=True)
 
 anchor.print_topics(F, collection.id_to_words, anc, 'ru_bank_wid_small/an+bi.txt')
 
@@ -16,6 +22,6 @@ F, T = plsa.plsa_model(
     collection,
     wrd_count=len(collection.id_to_words),
     metrics=[preplexity, coherence, uniq_top_of_topics],
-    num_iter=40, verbose=False, F=F)
+    num_iter=10, verbose=False, F=F)
 
 plsa.print_topics(F, collection.id_to_words, 'ru_bank_wid_small/an+pl+bi.txt')
