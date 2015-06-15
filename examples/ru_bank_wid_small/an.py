8da1da3ae0716e1a8809f093ef63e1b3e58b277e
Arseniy Ashuha <ars.ashuha@gmail.com>
2015-06-15 21:36:49 +0300

    remove bigramm tr from an

--- examples/ru_bank_wid_small/an.py
+++ examples/ru_bank_wid_small/an.py
@@ -3,13 +3,7 @@ from tmtk.topic_models import anchor
 from tmtk.metrics.metrics import preplexity, coherence, uniq_top_of_topics
 from tmtk.collection.collection import FullTextCollection
 
-from tmtk.collection.transformer_api import TransformerChainApply
-from tmtk.collection.transformer import BigramExtractorDocumentsTransform
-
 collection = FullTextCollection(path='./tmtk/corpa/ru_bank_wid_small.zip').fill()
-
-transformers = TransformerChainApply(transformers=[BigramExtractorDocumentsTransform()])
-collection = transformers.apply(collection)
 
 F, anc = anchor.anchor_model(
     collection, wrd_count=len(collection.id_to_words), metrics=[preplexity, coherence, uniq_top_of_topics])
