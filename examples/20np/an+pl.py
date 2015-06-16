371a13cfe159b6b4a676eb5f683515acc3b63a39
Arseniy Ashuha <ars.ashuha@gmail.com>
2015-06-16 08:40:08 +0300

    20 np collection res

--- examples/20np/an+pl.py
+++ examples/20np/an+pl.py
@@ -8,7 +8,7 @@ collection = FullTextCollection(path='./tmtk/corpa/20np.zip', lang='en').fill()
 F, anc = anchor.anchor_model(
     collection,
     wrd_count=len(collection.id_to_words),
-    metrics=[preplexity, coherence, uniq_top_of_topics])
+    metrics=[preplexity, coherence, uniq_top_of_topics], k=50)
 
 anchor.print_topics(F, collection.id_to_words, anc, '20np/an.txt')
 
--- examples/20np/an.py
+++ examples/20np/an.py
@@ -6,6 +6,5 @@ from tmtk.collection.collection import FullTextCollection
 collection = FullTextCollection(path='./tmtk/corpa/20np.zip', lang='en').fill()
 
 F, anc = anchor.anchor_model(
-    collection, wrd_count=len(collection.id_to_words), metrics=[preplexity, coherence, uniq_top_of_topics])
-
+    collection, wrd_count=len(collection.id_to_words), metrics=[preplexity, coherence, uniq_top_of_topics], k=50)
 anchor.print_topics(F, collection.id_to_words, anc, '20np/an.txt')
