730f190dad70f53aab951a13be4e0d001d55403d
Arseniy <ars-ashuha@yandex.ru>
2015-06-16 08:08:15 +0300

    20 np collection res

---
+++ examples/20np/an.py
@@ -0,0 +1,11 @@
+from tmtk.topic_models import anchor
+
+from tmtk.metrics.metrics import preplexity, coherence, uniq_top_of_topics
+from tmtk.collection.collection import FullTextCollection
+
+collection = FullTextCollection(path='./tmtk/corpa/20np.zip').fill()
+
+F, anc = anchor.anchor_model(
+    collection, wrd_count=len(collection.id_to_words), metrics=[preplexity, coherence, uniq_top_of_topics])
+
+anchor.print_topics(F, collection.id_to_words, anc, '20np/an.txt')
